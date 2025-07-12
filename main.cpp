b8dcc60b9742158b3653f2861b41160f7bbb4f2e
coconutDreams-56 <cabreraheily@gmail.com>
2025-07-11 21:13:30 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,9 @@
 //# Project-D---Problem-13.5---Heily-Cabrera
 
-// Turn the linked list implementation into a circular list: Have the previous pointer of the first node point to the last node, and the next pointer of the last node point to the first node. Then remove the last pointer in the List class because the value can now be obtained as first->previous. Reimplement the member functions so that they have the same effect as before. // 
+// Turn the linked list implementation into a circular list: 
+// Have the previous pointer of the first node point to the last node, and the next pointer of the last node point to the first node. 
+// Then remove the last pointer in the List class because the value can now be obtained as first->previous. 
+// Reimplement the member functions so that they have the same effect as before. // 
 
 //Programmer: Heily Cabrera
 //Tester: Audrey Tapia 
